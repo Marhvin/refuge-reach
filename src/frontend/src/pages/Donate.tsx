3c20874e162988062e5f127b4ec1bb5ac11ef1c7
Wilson Wei <2004wwei@gmail.com>
2024-11-02 19:18:49 -0400

    Update Donate.tsx

--- src/frontend/src/pages/Donate.tsx
+++ src/frontend/src/pages/Donate.tsx
@@ -1,10 +1,9 @@
 import { Button } from "../components/ui/button";
-import { Card } from "../components/ui/card";
 
 export default function Donate() {
   return (
     <div className="flex flex-col items-center justify-center gap-5 ">
-      <div className="bg-blue-500 text-white text-7xl font-bold py-[120px] px-[450px] rounded-lg transform transition-transform">
+      <div className="bg-blue-800 text-white text-7xl font-bold py-[120px] px-[450px] transform transition-transform">
         Donate today for a better tomorrow.
       </div>
       <Button className="text-2xl">Donate to Urban Refuge</Button>
