28859118d9edbdb016c7903f20173c4114020cdb
Alan <147638290+alaneng-neu@users.noreply.github.com>
2024-11-02 18:12:39 -0400

    connect hook to find page

--- src/frontend/src/app/App.tsx
+++ src/frontend/src/app/App.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "../pages/Home";
-import NotFound from "../pages/NotFound";
-import Dashboard from "../pages/Dashboard";
+import FindPage from "../pages/FindPage";
 import Donate from "../pages/Donate";
+
+import NotFound from "../pages/NotFound";
+import AppContextQuery from "./AppContextQuery";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/donate" element={<Donate />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <AppContextQuery>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/find" element={<FindPage />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </AppContextQuery>
   );
 };
 
