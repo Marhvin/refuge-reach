56aa9772a0fa104b395686bd088b4429d85d9e37
Wilson Wei <2004wwei@gmail.com>
2024-11-02 22:52:01 -0400

    Update Donate.tsx

--- src/frontend/src/pages/Donate.tsx
+++ src/frontend/src/pages/Donate.tsx
@@ -7,19 +7,19 @@ export default function Donate() {
     <>
       <div>
         <Navbar></Navbar>
-        <div className="flex flex-col items-center gap-1 ">
-          <div className="bg-blue-800 py-[30px] px-[450px] transform transition-transform">
-            <div className="flex items-center justify-betwee gap-5">
+        <div className="flex flex-col items-start justify-center">
+          <div className="bg-blue-800 py-[30px] px-[350px] transform transition-transform w-full">
+            <div className="flex items-center gap-16">
               <img
                 src="https://images.pexels.com/photos/10629451/pexels-photo-10629451.jpeg?auto=compress&cs=tinysrgb&w=800"
                 alt="Description"
-                className="size-3/5 rounded-lg"
+                className="w-1/3 rounded-lg"
               />
               <div className="flex-1">
                 <p className=" text-white text-7xl font-bold pb-12">
-                  Donate today for a better tomorrow.
+                  Donate today for a <u>better</u> tomorrow.
                 </p>
-                <Button className="text-lg bg-yellow-400 text-blue-800 font-semibold hover:bg-yellow-500 transition-colors px-8 py-8 rounded-lg shadow-lg hover:shadow-xl">
+                <Button className="text-lg bg-yellow-400 text-blue-800 font-semibold hover:bg-yellow-500 transition-colors px-5 py-5 -mt-16 rounded-lg shadow-lg hover:shadow-xl">
                   Donate to Urban Refuge
                 </Button>
               </div>
